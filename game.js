9ffdfadcf8d5806add1d5101777d7a9fd2e3dd75
cander465 <cander465@west-mec.org>
2025-11-19 17:49:30 -0700

    Added the first position draft
    This position function was researched on google and hopefully it works

--- game.js
+++ game.js
@@ -1,4 +1,34 @@
-const start = document.getElementById.apply(`start`)
+const start = document.getElementById(`start`)
+const reset = document.getElementById(`reset`)
+const gameArea = document.getElementById(`gameArea`)
+const target = document.getElementById(`target`)
+// basic targeters for the divs in the Html
+
+
+
+function position(target,gameArea){
+    const areaWidth = gameArea.clientWidth;
+    const areaHeight = gameArea.clientHeight;
+    const tgtHeight = target.clientHeight;
+    const tgtWidth = target.clientWidth;
+    // In my research of how to make a div a apppear 
+    // randomly inside of a div this was inlcuded.
+    // I looked it up if it wasnt obvious.
+    // It just defines widths and heights as numbers for math.
+
+    const maxX = areaWidth - tgtWidth;
+    const maxY = areaHeight - tgtHeight;
+
+    const randomX = Math.floor(Math.random() * maxX);
+    const randomY = Math.floor(Math.random() * maxY);
+
+    target.style.left = randomX + `px`;
+    target.style.top = randomY + `px`;
+}
+reset.addEventListener(`click`, e=>{
+    start.style.display = `block`;
+})
 start.addEventListener(`click`, (e)=>{
-    start.style.display = none;
+    start.style.display =`none`;
+    target(target,gameArea);    
 }) 
